6c55efec25c25d14cd3f3e15e9f274cc9c564e04
Tom Luo <mariolzx@gmail.com>
2021-03-06 00:31:52 -0500

    Initial Commit

---
+++ phonecall/phonecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  phonecallApp.swift
+//  phonecall
+//
+//  Created by Tom Lou on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct phonecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
