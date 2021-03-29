9d498bb5825f91182a90fb945c21c023778b507f
Tom Luo <mariolzx@gmail.com>
2021-03-29 06:09:04 -0400

    preview view

--- phonecall/ContentView.swift
+++ phonecall/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         return ZStack(content: {
-            RoundedRectangle(cornerRadius: 15.0).stroke()
-            Text("👻 Hello11877k5667").font(Font.largeTitle).padding()
+            RoundedRectangle(cornerRadius: 15.0).stroke().foregroundColor(Color/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Text("👻Hello11877k5d667").font(Font.largeTitle).padding()
         })
     }
 }
 
 
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        //Text("Hello, World!!")
+    }
+}
