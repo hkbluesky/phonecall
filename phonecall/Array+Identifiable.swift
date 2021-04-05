3d20d81a50c945b5bf7da530beb6fd685ad3a7de
Tom Luo <mariolzx@gmail.com>
2021-04-05 14:53:05 -0400

    working grid component

---
+++ phonecall/Array+Identifiable.swift
@@ -0,0 +1,20 @@
+//
+//  Array+Identifiable.swift
+//  phonecall
+//
+//  Created by Tom Lou on 4/5/21.
+//
+
+import Foundation
+
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int {
+        for index in 0..<self.count {
+            if self[index].id == matching.id {
+                return index
+            }
+        }
+        return 0
+    }
+}
