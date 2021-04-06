156637406f65c3c8775d68443114fa6eb9c399e8
Tom Luo <mariolzx@gmail.com>
2021-04-06 16:45:55 -0400

    Complete lecture 5

--- phonecall/EmojiMemoryGameView.swift
+++ phonecall/EmojiMemoryGameView.swift
@@ -49,28 +49,22 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     private func body(for size: CGSize) -> some View {
-        ZStack  {
-            if self.card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                //Circle().padding(5).opacity(0.4)
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                    .padding(5).opacity(0.4)
-                Text(self.card.content)
-            } else {
-                if !card.isMatached {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.yellow)
-                }
-            }
+        if card.isFaceUp || !card.isMatached {
+         ZStack  {
+            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                .padding(5).opacity(0.4)
+            Text(self.card.content)
+                .font(Font.system(size: fontSize(for: size)))
         }
-        .font(Font.system(size: fontSize(for: size)))
+        //.modifier(Cardify(isFaceUp: card.isFaceUp))
+        .cardify(isFaceUp: card.isFaceUp)
     }
+    }
+    
     // MARK: - Drawing Constants
     
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3
-    private let fontScaleFactor: CGFloat = 0.75
     
     func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.7
@@ -82,8 +76,11 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        let game = EmojiMemoryGame ()
-        game.choose(card: game.cards[3])
-        return EmojiMemoryGameView(viewModel: game)
+        //let game = EmojiMemoryGame ()
+        //game.choose(card: game.cards[3])
+        //return EmojiMemoryGameView(viewModel: game)
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+
     }
 }
+
